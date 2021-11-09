47269bdeb81dc7a5a3c33678f6e857636f5d7ce5
soopstar <probablysoup@protonmail.com>
2021-11-09 10:40:31 -0500

    1.0.5

--- the actual project/chatRoom.java
+++ the actual project/chatRoom.java
@@ -1,11 +1,35 @@
 import java.util.Scanner;
 
+// Might use to save data to a file. Not exactly sure of usage, though. JSON, maybe?
+import java.io.PrintWriter;
+
 public class chatRoom {
     public static void main(String[] args) {
+        // Create Nova the chat bot
+        Nova Nova = new Nova();
+
+        // Scanner for all user input
         Scanner input = new Scanner(System.in);
+
+        // String to hold user responses
+        String userAnswer;
         
         System.out.println("Hello, I'm Nova!");
+        System.out.print("Enter 'S' for settings.");
+        userAnswer = input.nextLine();
+
+        switch (userAnswer.charAt(0)) {
+            case 'S':
+                novaSettings();
+            default:
+
+        }
 
         input.close();
+    }
+
+    // Method to define settings for Nova
+    public static void novaSettings() {
+
     }
 }
