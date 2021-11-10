85e2858bff93bfc6ecdfadeeeecbeda35a65774a
soopstar <probablysoup@protonmail.com>
2021-11-09 19:13:04 -0500

    0.0.8

--- the actual project/Nova.java
+++ the actual project/Nova.java
@@ -1,15 +1,53 @@
 public class Nova {
     /**
      * A chat bot
+     * @param name The bot's name, set to "Nova" by default
      * @param personality A number relating to the personality type of the bot
-      */
+    */
 
+    private String name="Nova";
     private int personality;
 
+    // TODO: Maybe do this documentation better?
+    /**
+     * 
+     * @param n Name for the bot, specified by the user (String)
+     * @param p Personality
+     */
+
+    public Nova(String n, int p) {
+        name = n;
+        personality = p;
+    }
+
+    /**
+     * Default constructor for Nova
+     * @param p
+     */
     public Nova(int p) {
         personality = p;
     }
 
+    /**
+     * Used to get the name of the bot
+     * @return Returns the name of the bot
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Used to rename the bot
+     * @param n What to change the bot's name to
+     */
+    public void setName(String n) {
+        name = n;
+    }
+
+    /**
+     * Used to print out the personality of the bot
+     * @return A description personality of the bot
+     */
     public String getPersonality() {
         
         // TODO: Return a personality type for the bot depending the number
@@ -21,5 +59,13 @@ public class Nova {
             default:
             return "";
         }
+    }
+
+    /**
+     * Used to change the bot's personality
+     * @param p
+     */
+    public void setPersonality(int p) {
+        personality = p;
     }
 }
---
+++ the actual project/To-do.md
@@ -0,0 +1,9 @@
+# To-do
+
+## Really need to do
+- [ ] Finish Nova class
+
+- [ ] Allow user to create a nova bot when first starting the program
+
+## Maybe near future
+- [ ] Figure out how to locally save data for the bot
