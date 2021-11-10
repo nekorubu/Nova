93572ff8736df71aa810c0e697611c40f322b0f4
soopstar <probablysoup@protonmail.com>
2021-11-10 09:43:52 -0500

    0.0.9

--- the actual project/Nova.java
+++ the actual project/Nova.java
@@ -1,21 +1,33 @@
+import java.util.Scanner;
+
 public class Nova {
     /**
      * A chat bot
      * @param name The bot's name, set to "Nova" by default
      * @param personality A number relating to the personality type of the bot
+     * @param skipKey The key (character) used to print all the output at once until the next section
+     * @param skip Whether or not the bot is set to output everything at once
+     * @param alwaysSkip Whether or not the bot is set to always output everything at once
     */
 
     private String name="Nova";
     private int personality;
+    private char skipKey;
+    private boolean skip = false;
+    private boolean alwaysSkip = false;
+
+    private Scanner input = new Scanner(System.in);
+    private String userAnswer;
 
     // TODO: Maybe do this documentation better?
     /**
      * 
      * @param n Name for the bot, specified by the user (String)
      * @param p Personality
+     * @param s Key (character) to be used to print all output until last message at once
      */
 
-    public Nova(String n, int p) {
+    public Nova(String n, int p, char s) {
         name = n;
         personality = p;
     }
@@ -24,8 +36,9 @@ public class Nova {
      * Default constructor for Nova
      * @param p
      */
-    public Nova(int p) {
+    public Nova(int p, char s) {
         personality = p;
+        skipKey = s;
     }
 
     /**
@@ -67,5 +80,54 @@ public class Nova {
      */
     public void setPersonality(int p) {
         personality = p;
+    }
+
+    // TODO: Add documentation
+    public char getSkipKey() {
+        return skipKey;
+    }
+
+    // TODO: Add documentation
+    public void setSkipKey(char s) {
+        skipKey = s;
+    }
+
+    public void intro() {
+        System.out.println("Hello, I'm " + this.name + "!");
+    }
+
+    /**
+     * Print out text that is sent to it
+     * @param m Message to output
+     * @param last (Optional) Whether or not this is the last message to output this time
+     */
+    public void print(String m, boolean last) {
+        System.out.print(m);
+        if (!last) {
+            System.out.print("... ");
+        } else {
+            skip = false;
+        }
+        if (!skip) {
+            userAnswer = input.nextLine();
+            if (userAnswer.length()==1 && userAnswer.charAt(0)==skipKey) {
+                skip = true;
+            }
+        }
+    }
+
+    /**
+     * Print out text that is sent to it
+     * @param m Message to output
+     */
+    public void print(String m) {
+        System.out.print(m + "... ");
+
+        if (!skip) {
+            userAnswer = input.nextLine();
+            if (userAnswer.length()==1 && userAnswer.charAt(0)==skipKey) {
+                skip = true;
+            }
+        }
     }
 }
--- the actual project/chatRoom.java
+++ the actual project/chatRoom.java
@@ -6,7 +6,7 @@ import java.io.PrintWriter;
 public class chatRoom {
     public static void main(String[] args) {
         // Create Nova the chat bot
-        Nova Nova;
+        Nova Nova = new Nova(1, 's');
 
         // Scanner for all user input
         Scanner input = new Scanner(System.in);
@@ -14,7 +14,11 @@ public class chatRoom {
         // String to hold user responses
         String userAnswer;
         
-        System.out.println("Hello, I'm Nova!");
+        Nova.intro();
+
+        Nova.print("This a test");
+
+
         
         // TODO: Have user create a Nova
         
@@ -30,6 +34,8 @@ public class chatRoom {
 
         input.close();
     }
+
+    // TODO: Maybe move the conversation flow here
 
     // Method to define settings for Nova
     public static void novaSettings() {
